554a9363c9fd22817e9017069f9e82c4239f62ed
adamhaley <adamhaley@gmail.com>
2025-02-13 19:43:04 -0800

    loading PDF, printing

--- ragscraper.py
+++ ragscraper.py
@@ -1,3 +1,20 @@
 import os
+import shutil
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
 
-print("hello world, I'm here!")
+CHROMA_PATH = "chroma"
+DATA_PATH = "./data/e-mu_eos_4.0_manual.pdf"
+
+
+loader = PyPDFLoader(
+    file_path = DATA_PATH,
+    extract_images = False,
+    # headers = None
+    # extraction_mode = "plain",
+    # extraction_kwargs = None,
+)
+
+documents = loader.load()
+
+print(documents[0])
