63881e2d38892a0634d4879f82279153dfed7ed9
adamhaley <adamhaley@gmail.com>
2025-02-18 14:29:16 -0800

    successfully split, displaying chunked metadata in ui

--- ragscraper.py
+++ ragscraper.py
@@ -1,7 +1,8 @@
 import os
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_core.documents import Document
 from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
 import streamlit as st
@@ -13,8 +14,8 @@ CHROMA_PATH = "chroma"
 DATA_PATH = "./data/e-mu_eos_4.0_manual.pdf"
 
 
-pages = pymupdf.open(DATA_PATH)
-
+loader = PyMuPDFLoader(DATA_PATH)
+docs = loader.load()
 
 #db = Chroma.from_documents(texts, embeddings)
 
@@ -25,9 +26,23 @@ ask = st.button(
     "🔥 Ask",
 )
 
-for page in pages: # iterate the document pages
-    print(page)
-    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
-    print(text) # write text of page
-    print(bytes((12,))) # write page delimiter (form feed 0x0C)
+
+#for page in pages: # iterate the document pages
+#    print(page)
+#    text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
+#    print(text) # write text of page
+#    print(bytes((12,))) # write page delimiter (form feed 0x0C)
+
+
+#text split the doc, chunk and embed
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400,
+        chunk_overlap=100,
+        separators=["\n\n","\n",".","?","!"," ",""],
+    )
+
+split_doc = text_splitter.split_documents(docs)
+
+st.write(split_doc)
+
 
