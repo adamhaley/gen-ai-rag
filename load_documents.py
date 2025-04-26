828a5d84e6457ecb2b328cb205e6cd38c64c2715
adamhaley <adamhaley@gmail.com>
2025-04-25 18:25:11 -0700

    adding text splitting example code

--- load_documents.py
+++ load_documents.py
@@ -9,6 +9,7 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 from langchain_community.document_loaders import PyPDFLoader
 
+#LOAD DOCUMENT
 DATA_PATH = "./data/e-mu_eos_4.0_manual.pdf"
 
 try: 
@@ -21,5 +22,25 @@ print(len(pages))
 
 page = pages[0]
 
+
+#PRINT OUT SOME DATA PROVING LOADING WORKED
 print(page.page_content[:1500])
 print(page.metadata)
+
+#SPLIT THE TEXT
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+
+chunk_size =26
+chunk_overlap = 4
+
+r_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap
+)
+c_splitter = CharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap
+)
+
+
+
