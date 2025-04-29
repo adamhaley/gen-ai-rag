a9db7d8454a382ed67a430a6906f98dcbb35ace9
adamhaley <adamhaley@gmail.com>
2025-04-29 11:16:52 -0700

    added excecution time to embeddings test, added openai to compare openai / local ollama

--- embeddings.py
+++ embeddings.py
@@ -1,14 +1,31 @@
 import os
 import openai
 import sys
+import time
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
+from langchain_openai import OpenAIEmbeddings
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+start_time = time.perf_counter()
 
 embedding = OllamaEmbeddings(model="mistral")
+
+'''
+embedding = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    # With the `text-embedding-3` class
+    # of models, you can specify the size
+    # of the embeddings you want returned.
+    # dimensions=1024
+)
+'''
 
 #Using numpy dot product to compare vector similarity
 sentence1 = "Friday is my favorite day of the week."
@@ -24,5 +41,10 @@ import numpy as np
 print(np.dot(embedding1, embedding2))
 print(np.dot(embedding1, embedding3))
 print(np.dot(embedding2, embedding3))
+
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time:.4f} seconds")
+
 
 
